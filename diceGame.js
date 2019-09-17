d9349bc1f25b22abe1230f5892ad9bad8db6bcc7
trevor clements <trevortheamazing@gmail.com>
2019-09-17 08:32:04 -0500

    gameplay logic.

--- diceGame.js
+++ diceGame.js
@@ -12,7 +12,8 @@ var diceArray = [];
 
 
 function initializeDiceArray(arrayOfDice) {
-    for (let i = 0; i < 6; i++) {
+    //for (let i = 0; i < 6; i++) {
+    for (let i = 0; i <= (diceCountInput - 1); i++) {
         //let die = { dieName: "d" + (i + 1), sideCount: 6, pipCount: i + 1, keepStatus: false };
         let die = { dieName: "d" + (i + 1), sideCount: sideCountInput, pipCount: i + 1, keepStatus: false };
         arrayOfDice.push(die);
@@ -52,14 +53,26 @@ function diceRoll(arrayOfDice) {
     return arrayOfDice;
 }
 
+//set the keepStatus to prevent rolling a dice that is 'kept'
+function setKeepStatus(arrayOfDice) {
+    for (let i = 0; i < arrayOfDice.length; i++) {
+        //console.log(arrayOfDice[i].dieName + " is showing " + arrayOfDice[i].pipCount + " after the roll.");
+        alert(arrayOfDice[i].dieName + " is showing " + arrayOfDice[i].pipCount + " after the roll.");
+        arrayOfDice[i].keepStatus = (prompt("Would you like to keep? y/n") === 'y');
+    }
+}
+
 
 //clear the console
 console.clear();
 
+var diceCountInput = 0;
 var sideCountInput = 0;
+
+diceCountInput = Number(prompt("How many dice will you be playing with?"));
 sideCountInput = Number(prompt("How many sides shall your dice have?"));
 
-if (sideCountInput == 6) {
+if ((diceCountInput == 5) && (sideCountInput == 6)) {
     if (prompt("Would you like to play a game that is not unlike 'Yahtzee?'") === 'y') {
         alert("call playGame(realDeal)");
     }
@@ -81,3 +94,25 @@ diceReport(diceArray);
 diceArray = diceRoll(diceArray, diceArray.length);
 console.log("Here is the new status of your dice:");
 diceReport(diceArray);
+
+//set the keepStatus to prevent rolling a dice that is 'kept'
+setKeepStatus(diceArray);
+
+//score the kept dice as the sum of the kept dice after three turns.
+//log the turn number.
+//add more conditions later for: multiples(2x,3x,5x), runs
+//log 'game over' and announe the score
+
+if (confirm("") === true) {
+    for (let i = 0; i <= 3; i++) {
+        //roll
+        //keep?
+    }
+    //score
+}
+
+//game over
+
+// playGame(gameMode){
+//     }
+
